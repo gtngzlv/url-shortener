c019c231bfebeaad4afbb35982ae3fdc3538e274
guzel.gatina <guzel.gatina@dif.tech>
2023-11-17 17:20:00 +0200

    iter23

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,13 @@
 package app
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"golang.org/x/crypto/acme/autocert"
@@ -14,25 +20,52 @@ import (
 )
 
 // Run starts application
-func Run() error {
+func Run() {
+	srv, err := runSrv()
+	if err != nil {
+		log.Fatalf("Failed to create HTTP server: %v", err)
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe error: %v", err)
+		}
+	}()
+
+	sig := <-sigint
+	log.Printf("Received signal: %v", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("HTTP server Shutdown error: %v", err)
+	}
+}
+
+func runSrv() (*http.Server, error) {
 	router := chi.NewRouter()
 	log := logger.NewLogger()
 	cfg := config.LoadConfig()
 	st := storage.Init(log, cfg)
 	app := handlers.NewApp(router, cfg, log, st)
+
+	srv := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: router,
+	}
+
 	if cfg.EnableHTTPS {
 		manager := &autocert.Manager{
 			Cache:      autocert.DirCache("cache-dir"),
 			Prompt:     autocert.AcceptTOS,
 			HostPolicy: autocert.HostWhitelist(cfg.ServerAddress),
 		}
-		srv := &http.Server{
-			Addr:      cfg.ServerAddress,
-			Handler:   router,
-			TLSConfig: manager.TLSConfig(),
-		}
-		return srv.ListenAndServeTLS("", "")
+		srv.TLSConfig = manager.TLSConfig()
+		return srv, srv.ListenAndServeTLS("", "")
 	}
-	return http.ListenAndServe(cfg.ServerAddress, app.Router)
-
+	return srv, http.ListenAndServe(cfg.ServerAddress, app.Router)
 }
